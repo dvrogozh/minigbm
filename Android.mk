c495ff322f4416b8c7f3036c978d7dec4e3a1a97
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-10-11 00:35:46 -0700

    experiment
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- Android.mk
+++ Android.mk
@@ -2,6 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# experiment
+
+
 ifeq ($(strip $(BOARD_USES_MINIGBM)), true)
 
 MINIGBM_GRALLOC_MK := $(call my-dir)/Android.gralloc.mk
